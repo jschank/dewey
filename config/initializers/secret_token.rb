2ba833da67cce2bf19be98e18792aafa72d464ba
John F. Schank III <jschank@mac.com>
2011-09-17 15:49:51 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dewey::Application.config.secret_token = 'a0e9f94ab6b8789865a830576a0e866238ad018e715e1dee81a164e66e64266a362add2c40275fbd7073cb5b8a3db7f6c8edd0aaca88c9eff2d3d068bddacf83'
