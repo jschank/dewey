947dfe0707d7a6dd2c123891b2b8f501141bf2e3
Mike Stone <mtnclmbr@mac.com>
2012-05-13 13:53:56 -0400

    changed a couple config file so Devise works on Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,4 +57,7 @@ Dewey::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+
+  config.action_mailer.default_url_options = { :host => 'http://idewit.herokuapp.com/' }
+
 end
