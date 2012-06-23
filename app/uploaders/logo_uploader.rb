81dd67dd71db4ef96acc016e6bdd05afce1a0b0a
John F. Schank III <jschank@mac.com>
2012-06-23 18:03:34 -0400

    No longer using the really long resource names for the images

--- app/uploaders/logo_uploader.rb
+++ app/uploaders/logo_uploader.rb
@@ -8,14 +8,14 @@ class LogoUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
-  # storage :fog
+  # storage :fog  
   storage :s3
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-    # "uploads/#{model.class.to_s.underscore}"
+    # "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "uploads/#{model.class.to_s.underscore}"
   end
   
   def cache_dir
