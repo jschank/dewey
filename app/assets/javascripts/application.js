b1c01391712eeaef89c2c47f9c10388457c3adec
John F. Schank III <jschank@mac.com>
2012-07-28 17:10:41 -0400

    Changed the order in which dynamic fields are appended to the div in which they live

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,7 +17,7 @@ function remove_fields(link) {
 function add_fields(link, association, content) {
         var new_id = new Date().getTime();
         var regexp = new RegExp("new_" + association, "g");
-        $(link).parent().after(content.replace(regexp, new_id));
+        $(link).before(content.replace(regexp, new_id));
 }
 
 function populate_schedulables(chooser)
