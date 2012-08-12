8b5553b29b82e6d3c8dea4671e455633d1dc1f89
John F. Schank III <jschank@mac.com>
2012-08-12 16:40:26 -0400

    Added page sensitive coloring on tabbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,14 @@
 class ApplicationController < ActionController::Base
   helper_method :current_time
+  helper_method :page_class
   protect_from_forgery
 
   def current_time
     Rails.env == 'development' ? DateTime.civil(2012, 04, 13, 21, 30, 0, "-4") : DateTime.now
+  end
+  
+  def page_class(page)
+    str = "#{page.to_s}_#{params[:controller] == page ? 'on' : 'off'}"
   end
 
 end
