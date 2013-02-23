ce0866c285a920c14b9a6b656300a250a35a7dc7
John F Schank III <jschank@mac.com>
2013-02-23 17:07:59 -0500

    Acts index now isolates to events that are scheduled with the last 6 months or future

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -3,7 +3,8 @@ class EventsController < ApplicationController
   # GET /events
   # GET /events.json
   def index
-    @events = Event.all
+    # @events = Event.all
+    @events = Schedule.upcoming(current_time - 6.months).of_type(Event).map{|item| item.schedulable}.uniq
 
     respond_to do |format|
       format.html 
