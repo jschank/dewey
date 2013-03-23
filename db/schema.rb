76de097a9879205966d7aa10a4d9e0c281d8d635
Mike Stone <mtnclmbr@mac.com>
2013-03-23 10:05:48 -0400

    More style changes including the back, public, and admin buttons.

--- db/schema.rb
+++ db/schema.rb
@@ -13,71 +13,38 @@
 
 ActiveRecord::Schema.define(:version => 20130302182405) do
 
-  create_table "active_admin_comments", :force => true do |t|
-    t.string   "resource_id",   :null => false
-    t.string   "resource_type", :null => false
-    t.integer  "author_id"
-    t.string   "author_type"
-    t.text     "body"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "namespace"
-  end
-
-  add_index "active_admin_comments", ["author_type", "author_id"], :name => "index_active_admin_comments_on_author_type_and_author_id"
-  add_index "active_admin_comments", ["namespace"], :name => "index_active_admin_comments_on_namespace"
-  add_index "active_admin_comments", ["resource_type", "resource_id"], :name => "index_admin_notes_on_resource_type_and_resource_id"
-
   create_table "acts", :force => true do |t|
     t.string   "name"
     t.text     "description"
     t.string   "hometown"
     t.string   "formerly"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
     t.string   "logo"
   end
-
-  create_table "admin_users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
-  add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
 
   create_table "details", :force => true do |t|
     t.string   "name"
     t.string   "description"
     t.integer  "schedule_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                    :null => false
+    t.datetime "updated_at",                    :null => false
     t.integer  "order",       :default => 1000
   end
 
   create_table "events", :force => true do |t|
     t.string   "name"
     t.text     "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
     t.string   "logo"
   end
 
   create_table "festivals", :force => true do |t|
     t.string   "name"
     t.text     "description"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
     t.datetime "start"
     t.datetime "end"
     t.string   "logo"
@@ -88,15 +55,15 @@ ActiveRecord::Schema.define(:version => 20130302182405) do
     t.string   "linkable_type"
     t.integer  "weblocation_id"
     t.string   "url"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
   end
 
   create_table "locations", :force => true do |t|
     t.integer  "venue_id"
     t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   add_index "locations", ["venue_id"], :name => "index_locations_on_venue_id"
@@ -104,8 +71,8 @@ ActiveRecord::Schema.define(:version => 20130302182405) do
   create_table "related_acts", :force => true do |t|
     t.integer  "act_1_id"
     t.integer  "act_2_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "schedules", :force => true do |t|
@@ -115,8 +82,8 @@ ActiveRecord::Schema.define(:version => 20130302182405) do
     t.datetime "end"
     t.integer  "location_id"
     t.integer  "parent_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                         :null => false
+    t.datetime "updated_at",                         :null => false
     t.integer  "order",            :default => 1000
     t.integer  "festival_id"
     t.boolean  "display_time",     :default => true
@@ -133,8 +100,8 @@ ActiveRecord::Schema.define(:version => 20130302182405) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
@@ -149,15 +116,15 @@ ActiveRecord::Schema.define(:version => 20130302182405) do
     t.string   "state"
     t.string   "zip"
     t.string   "phone"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
     t.string   "logo"
   end
 
   create_table "weblocations", :force => true do |t|
     t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                  :null => false
+    t.datetime "updated_at",                  :null => false
     t.string   "logo"
     t.integer  "popularity", :default => 100
   end
